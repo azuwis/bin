bc61544f318bdd0e3c220b1bfe96c0c86d69130a
Zhong Jianxin (azuwis) <azuwis@gmail.com>
2012-12-04 15:44:30 +0800

    deviantart-wallpaper: ajust output

--- deviantart-wallpaper.sh
+++ deviantart-wallpaper.sh
@@ -12,27 +12,29 @@ max_index=0
 max_width=0
 for url in `xmlstarlet sel --net -T -t -m 'rss/channel/item/media:content[@medium="document"]' -v @url -n $rss`
 do
-    echo "working with #$index item"
     # work with the first 24 items
     if [ $index -gt 24 ]; then
         break
     fi
+    echo -n "downloading #$index item..."
     curl -sL $url >${index}.jpg
     width=`feh -L %w ${index}.jpg 2>/dev/null`
     if [ x"$width" = x"" ]; then
         width=0
     fi
+    echo -n "width $width"
     # if width > 1920, use it
     if [ $width -gt 1920 ]; then
         max_index=$index
-        echo "current widest item: #$index $max_width"
+        echo -n "(>1920)"
         break
     # else get the widest one
     elif [ $width -gt $max_width ]; then
         max_width=$width
         max_index=$index
-        echo "current widest item: #$index $max_width"
+        echo -n "(widest)"
     fi
+    echo
     index=$((index+1))
 done
 mv current previous
