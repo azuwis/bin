cb97be4f158696668ccf2c2c85f59d2eca8e6c6c
Zhong Jianxin <azuwis@gmail.com>
2013-03-11 16:24:36 +0800

    deviantart-wallpaper: store wallpapers in archive dir

--- deviantart-wallpaper.sh
+++ deviantart-wallpaper.sh
@@ -3,9 +3,16 @@
 # author: Zhong Jianxin (azuwis)
 # dep: xmlstarlet curl feh
 
-# Popular  Abstract in the last 1 day http://browse.deviantart.com/customization/wallpaper/abstract/
+set -e
+
+if [ x"$1" == x"prev" ]; then
+    feh --bg-fill ~/.wallpaper/previous
+    exit 0
+fi
+
+# Popular Abstract in the last 1 day http://browse.deviantart.com/customization/wallpaper/abstract/
 rss='http://backend.deviantart.com/rss.xml?q=boost%3Apopular+in%3Acustomization%2Fwallpaper%2Fabstract+max_age%3A24h&type=deviation'
-mkdir -p ~/.wallpaper
+mkdir -p ~/.wallpaper/archive
 cd ~/.wallpaper
 index=0
 max_index=0
@@ -26,7 +33,7 @@ do
     # if width > 1920, use it
     if [ $width -gt 1920 ]; then
         max_index=$index
-        echo -n "(>1920)"
+        echo "(>1920)"
         break
     # else get the widest one
     elif [ $width -gt $max_width ]; then
@@ -37,7 +44,11 @@ do
     echo
     index=$((index+1))
 done
-mv current previous
-mv ${max_index}.jpg current
+DATE=`date +%Y%m%d%H%M%S`
+mv ${max_index}.jpg archive/$DATE.jpg
+echo "previous -> `readlink current`"
+ln -sf `readlink current` previous
+echo "current -> archive/$DATE.jpg"
+ln -sf archive/$DATE.jpg current
 feh --bg-fill current
-rm *.jpg
+rm -f *.jpg
