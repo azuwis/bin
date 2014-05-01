5d180c640fe4071344d6a5f938c485c427fabd65
Zhong Jianxin <azuwis@gmail.com>
2014-05-01 23:45:40 +0800

    Update workspace-nanoc.sh

--- workspace-nanoc.sh
+++ workspace-nanoc.sh
@@ -5,7 +5,6 @@ firefox -no-remote -P dev http://localhost:3000 >&/dev/null &
 
 # tmux
 tmux new-session -d -s nanoc "nanoc view"
-sleep 1
 tmux split-window "guard"
 
 # move/resize firefox
@@ -16,7 +15,7 @@ i3-msg resize grow width 10 px or 10 ppt
 # vim
 i3-msg focus right
 i3-msg split v
-gvim Rules &
+gvim . &
 sleep 1
 i3-msg resize grow height 10 px or 30 ppt
 
